0560e3b341bcb949f3c18914922adb3e4895a8ba
Julian <julian@certora.com>
2025-12-17 08:57:12 -0500

    Initial Commit

---
+++ WALA CAst Solidity/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WALA CAst Solidity
+//
+//  Created by Julian Dolby on 12/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
